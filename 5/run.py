88434f766f294f6792a8931bdf599136bb1cfb0c
gbcham1 <c.h@a.com>
2022-12-06 13:13:07 +0100

    refactoring

--- 5/run.py
+++ 5/run.py
@@ -15,33 +15,24 @@ def load_crates():
             i += 1
     return crates
 
-def solution_1():
-    def move_crate(a, b):
-        crate = crates[a].pop()
-        crates[b].append(crate)
-
+def solution(s):
     for move in moves.splitlines():
         n, a, b = (int(x) for x in move.split()[1::2])
-        for i in range(n):
-            move_crate(a, b)
-    print("Top Crates: %s" % ''.join([stack[-1] for stack in crates.values()]))
-
-def solution_2():
-    def move_multiple_crates(n, a, b):
         movers = []
-        for crate in range(n):
+        for i in range(n):
             movers.append(crates[a].pop())
-        for crate in movers[::-1]:
-            crates[b].append(crate)
-
-    for move in moves.splitlines():
-        n, a, b = (int(x) for x in move.split()[1::2])
-        move_multiple_crates(n, a, b)
+        if s == 1:
+            for crate in movers:
+                crates[b].append(crate)
+        elif s == 2:
+            for crate in movers[::-1]:
+                crates[b].append(crate)
     print("Top Crates: %s" % ''.join([stack[-1] for stack in crates.values()]))
+
 
 if __name__=="__main__":
     crates = load_crates()
-    solution_1()
+    solution(1)
 
     crates = load_crates()
-    solution_2()
+    solution(2)
